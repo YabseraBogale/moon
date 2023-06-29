612eeeaf11f02379d73cb6f4db78edcb3f6a43e0
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-06-29 19:18:39 +0300

    Add files via upload

---
+++ length of table.lua
@@ -0,0 +1,16 @@
+t={
+	[3]="three",
+	[1]="one",
+	[2]="two"
+}
+
+
+print( "location of table in the memory",t)
+print("Length of data in table",#t)
+print(t[1])
+print(t[2])
+print(t[3])
+
+print(t[1]~="two")
+print(t[2]~="two")
+print(t[3]~="two")
